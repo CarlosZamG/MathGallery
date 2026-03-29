8623abec07345ed2345dc2aa2c9b944ad05c9e2c
Carlos <cdzamora02@outlook.es>
2026-03-29 08:22:58 -0600

    Fix Open Ball applet

--- open_ball.js
+++ open_ball.js
@@ -3,7 +3,7 @@ JXG.Options.text.useMathJax = true;
 
 var edge_open_ball = 4.5;
 
-var board = JXG.JSXGraph.initBoard('open_ball', {
+var board_open_ball = JXG.JSXGraph.initBoard('open_ball', {
     boundingbox: [-edge_open_ball, edge_open_ball, edge_open_ball, -edge_open_ball],
     showCopyright:false,
     axis:false,
@@ -18,40 +18,40 @@ var board = JXG.JSXGraph.initBoard('open_ball', {
     showScreenshot:true
 });
 
-var r = 4;
-var x = board.create('point',[0,0], {name:'$x$',size: 4, face: 'o', color:'#B57DFF', fixed:true});
-var open_ball = board.create('circle',[x,[r,0]], {strokeWidth:3, dash:2, strokeColor:'#B57DFF', fillColor:'#B57DFF', fillOpacity:0.1});
-var y = board.create('point',[-2,2], {name:'$y$',size: 4, face: 'o', color:'#7DAAFF'});
-var r1 = board.create('segment', [x, [r,0]], {name:'$r$', color:'#B57DFF', withLabel:true});
-var dxy = board.create('segment', [x, y], {name:'$d(x,y)$', color:'#FF7D7D', withLabel:true});
-var r2 = board.create('segment', [y, [function(){
-                                                var dist = Math.sqrt(y.X() ** 2 + y.Y() ** 2);
-                                                return r/dist*y.X();
+var r_open_ball = 4;
+var x_open_ball = board_open_ball.create('point',[0,0], {name:'$x$',size: 4, face: 'o', color:'#B57DFF', fixed:true});
+var open_ball = board_open_ball.create('circle',[x_open_ball,[r_open_ball,0]], {strokeWidth:3, dash:2, strokeColor:'#B57DFF', fillColor:'#B57DFF', fillOpacity:0.1});
+var y_open_ball = board_open_ball.create('point',[-2,2], {name:'$y$',size: 4, face: 'o', color:'#7DAAFF'});
+var r1_open_ball = board_open_ball.create('segment', [x_open_ball, [r_open_ball,0]], {name:'$r$', color:'#B57DFF', withLabel:true});
+var dxy_open_ball = board_open_ball.create('segment', [x_open_ball, y_open_ball], {name:'$d(x,y)$', color:'#FF7D7D', withLabel:true});
+var r2_open_ball = board_open_ball.create('segment', [y_open_ball, [function(){
+                                                var dist = Math.sqrt(y_open_ball.X() ** 2 + y_open_ball.Y() ** 2);
+                                                return r_open_ball/dist*y_open_ball.X();
                                             },
                                         function(){
-                                                var dist = Math.sqrt(y.X() ** 2 + y.Y() ** 2);
-                                                return r/dist*y.Y();
+                                                var dist = Math.sqrt(y_open_ball.X() ** 2 + y_open_ball.Y() ** 2);
+                                                return r_open_ball/dist*y_open_ball.Y();
                                             }
                                         ]], {name:'$r_2$', color:'#7DAAFF', withLabel:true});
 
 
 
-var inner_ball = board.create('circle', [y, [function(){return y.X()},
+var inner_ball = board_open_ball.create('circle', [y_open_ball, [function(){return y_open_ball.X()},
                                             function(){
-                                                var dist = Math.sqrt(y.X() ** 2 + y.Y() ** 2);
-                                                var new_r = r - dist;
-                                                return y.Y() + new_r;
+                                                var dist = Math.sqrt(y_open_ball.X() ** 2 + y_open_ball.Y() ** 2);
+                                                var new_r = r_open_ball - dist;
+                                                return y_open_ball.Y() + new_r;
                                             }
 ]], {strokeWidth:3, dash:2, strokeColor:'#7DAAFF', fillColor:'#7DAAFF', fillOpacity:0.2} )
 
 
-board.on('move', function(){
-    var xx = y.X();
-    var yy = y.Y();
+board_open_ball.on('move', function(){
+    var xx = y_open_ball.X();
+    var yy = y_open_ball.Y();
     var d = Math.sqrt(xx ** 2 + yy ** 2);
-    if( d > 0.9*r){
-        board.suspendUpdate();
-        y.moveTo([0.9*r*xx / d, 0.9*r*yy / d]);
-        board.unsuspendUpdate();
+    if( d > 0.9*r_open_ball ){
+        board_open_ball.suspendUpdate();
+        y_open_ball.moveTo([0.9*r_open_ball*xx / d, 0.9*r_open_ball*yy / d]);
+        board_open_ball.unsuspendUpdate();
     }
 })
