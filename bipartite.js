5d559b37527571bb5d4e8fb88679c69c48b883e5
Carlos <cdzamora02@outlook.es>
2026-04-03 15:13:20 -0600

    Fix elements of bipartite and perceptron applets

--- bipartite.js
+++ bipartite.js
@@ -30,45 +30,45 @@ function bipartite_applet(board)
     for (let index = 0; index < n; index++)
     {
 
-        var a1 = board.create('point', [x, 2], {visible:false})
-        var a2 = board.create('point', [x, 1.5], {visible:false})
+        var a1 = board.create('point', [x, 2], {visible:false, fixed:true})
+        var a2 = board.create('point', [x, 1.5], {visible:false, fixed:true})
 
-        var b1 = board.create('point', [x, -2], {visible:false})
-        var b2 = board.create('point', [x,-1.5], {visible:false})
+        var b1 = board.create('point', [x, -2], {visible:false, fixed:true})
+        var b2 = board.create('point', [x,-1.5], {visible:false, fixed:true})
 
         A[index] = a1;
         B[index] = b1
 
-        var v1 = board.create('circle',[a1, a2], {strokeWidth:3, color:'#000'});
+        var v1 = board.create('circle',[a1, a2], {strokeWidth:3, color:'#000', fixed:true});
         V1[index] = v1
         
-        var v2 = board.create('circle',[b1, b2], {strokeWidth:3, color:'#000'});
+        var v2 = board.create('circle',[b1, b2], {strokeWidth:3, color:'#000', fixed:true});
         V2[index] = v2
 
 
         for(let j = 0; j < index; j++)
         {
-            var s = board.create('segment',[A[j], b1], {strokeWidth:3, color:'#000'});
+            var s = board.create('segment',[A[j], b1], {strokeWidth:3, color:'#000', fixed:true});
         }
 
         x += 2;
     }
 
-    var ell1 = board.create('ellipse', [[-5, -2], [5, -2], [0, -3.2]], {fillColor:'#7DAAFF', strokeColor:'#7DAAFF'})
+    var ell1 = board.create('ellipse', [[-5, -2], [5, -2], [0, -3.2]], {fillColor:'#7DAAFF', strokeColor:'#7DAAFF', fixed:true})
 
-    var ell2 = board.create('ellipse', [[-5, 2], [5, 2], [0, 3.2]], {fillColor:'#B57DFF', strokeColor:'#B57DFF', opacity: 0.5 })
+    var ell2 = board.create('ellipse', [[-5, 2], [5, 2], [0, 3.2]], {fillColor:'#B57DFF', strokeColor:'#B57DFF', opacity: 0.5, fixed:true})
 
-    var s1 = board.create('segment',[A[1], B[0]], {strokeWidth:3, color:'#000'});
+    var s1 = board.create('segment',[A[1], B[0]], {strokeWidth:3, color:'#000', fixed:true});
 
-    var s2 = board.create('segment',[A[n-1], B[0]], {strokeWidth:3, color:'#000'});
+    var s2 = board.create('segment',[A[n-1], B[0]], {strokeWidth:3, color:'#000', fixed:true});
 
-    var s3 = board.create('segment',[A[n-1], B[n-1]], {strokeWidth:3, color:'#000'});
+    var s3 = board.create('segment',[A[n-1], B[n-1]], {strokeWidth:3, color:'#000', fixed:true});
 
-    var s4 = board.create('segment',[A[n-1], B[n-3]], {strokeWidth:3, color:'#000'});
+    var s4 = board.create('segment',[A[n-1], B[n-3]], {strokeWidth:3, color:'#000', fixed:true});
 
-    var textX = board.create('text', [0, -4, "$Y$"],{color:'#5583d7', fontSize: 18});
+    var textX = board.create('text', [0, -4, "$Y$"],{color:'#5583d7', fontSize: 18, fixed:true});
 
-    var textX = board.create('text', [0, 4, "$X$"],{color:'#B57DFF', fontSize: 18});
+    var textX = board.create('text', [0, 4, "$X$"],{color:'#B57DFF', fontSize: 18, fixed:true});
 
 }
 
--- perceptron.js
+++ perceptron.js
@@ -26,7 +26,7 @@ function perceptron_applet(board)
         var wlabel = '$w_'+ i + '$'
         var xlabel = '$x_'+ i + '$'
 
-        var s = board.create('segment', [[1,0], [-4, y - 0.5]], {layer:5, name:wlabel, withLabel:true, label:{offset:[2*y-4, 5*y]}});
+        var s = board.create('segment', [[1,0], [-4, y - 0.5]], {layer:5, name:wlabel, fixed:true, withLabel:true, label:{offset:[2*y-4, 5*y]}});
         var p1 = board.create('point', [-4.0, y], {visible: false});
         var p2 = board.create('point', [-5.0, y], {visible: false});
         var pol = board.create('regularpolygon', [p1, p2, 4], {color: '#69c384', withLines:false, name:xlabel, withLabel:true, opacity:1, label:{offset:[-4, 0]}});
@@ -36,17 +36,17 @@ function perceptron_applet(board)
 
     }
 
-    var neuron = board.create('circle', [[1,0], 2.1], {color:'#e7aaff', layer:10});
+    var neuron = board.create('circle', [[1,0], 2.1], {color:'#e7aaff', layer:10, fixed:true});
 
-    var sumtext = board.create('text', [-0.1, 0.3, '$z =w^{\\top} x + b=$'],{fontSize: 10})
-    var sumtext2 = board.create('text', [-0.9, -0.2, '$w_0\\cdot x_0 + \\dots + w_5\\cdot x_5+b$'],{fontSize: 10})
+    var sumtext = board.create('text', [-0.1, 0.3, '$z =w^{\\top} x + b=$'],{fontSize: 10, fixed:true})
+    var sumtext2 = board.create('text', [-0.9, -0.2, '$w_0\\cdot x_0 + \\dots + w_5\\cdot x_5+b$'],{fontSize: 10, fixed:true})
 
     var output = board.create('regularpolygon', [[4, -0.5], [5,-0.5], 4], {color: '#62a3e8', withLines:false, name:'$H(z)$', withLabel:true, layer:10, opacity:1, label:{offset:[-15, 0]}})
     output.vertices[0].setAttribute({visible: false});
     output.vertices[1].setAttribute({visible: false});
     output.vertices[2].setAttribute({visible: false});
     output.vertices[3].setAttribute({visible: false});
-    var l1 = board.create('arrow', [[0,0], [6,0]], {layer:5});
+    var l1 = board.create('arrow', [[0,0], [6,0]], {layer:5, fixed:true});
 }
 
 perceptron_applet(board_perceptron);
